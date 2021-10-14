40ffb1bc16bdb4f4a6e36b91006e01d4b1979136
louiscklaw <louiscklaw@gmail.com>
2021-10-14 17:00:02 +0800

    update,

---
+++ quickstart/rebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker-compose kill
+docker-compose down --remove-orphans
+
+docker network create web || true
+
+
+docker-compose up -d
+
+# docker-compose up -d --scale whoami=2
