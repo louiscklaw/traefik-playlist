d5f5d991ad6033827b85fc624de9a53101997ec6
louiscklaw <louiscklaw@gmail.com>
2022-05-16 18:01:40 +0800

    update

--- helloworld/up.sh
+++ helloworld/up.sh
@@ -2,15 +2,4 @@
 
 set -ex
 
-docker network create web || true
-
-docker-compose kill
-docker-compose rm -f
-docker-compose down -v --remove-orphans
-
-docker-compose build
-
-# docker-compose up -d
-docker-compose -f ./whoami.yml up -d
-docker-compose -f ./traefik.yml up -d
-# docker-compose up -d --scale whoami=2
+docker-compose up
