0b29c8cfbcfe54aaddd583b40e0666a175215780
louiscklaw <louiscklaw@users.noreply.github.com>
2023-07-12 15:12:12 +0800

    update traefik-production,

--- production/test.sh
+++ production/test.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
+
+docker kill $(docker ps -qa)
+docker rm $(docker ps -qa)
+
+sleep 1
+
+sudo rm -rf ./volumes/letsencrypt/*
+
 set -ex
+
 
 docker compose kill 
 docker compose down
